9c59f3b8ff2e39c0721720524bf35dc8fd26a453
Bronils <nonstop.hacking.free@gmail.com>
2020-03-04 00:34:12 +0700

    Upload

---
+++ setup/replicaSet.js
@@ -0,0 +1,7 @@
+rsconf = {_id : "rs0",members: [{ _id : 0, host : "mongo-rs0-1:27017" },{ _id : 1, host : "mongo-rs0-2:27017" },{ _id : 2, host : "mongo-rs0-3:27017" },]};
+
+rs.initiate(rsconf);
+
+cfg = rs.conf();
+cfg.members[0].priority = 2;
+rs.reconfig(cfg);
